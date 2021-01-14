a02bca8c91943c1afd447abd00d0009eb7efb401
youheikanno <tkro.812.wkrni@icloud.com>
2021-01-14 11:54:08 +0900

    enumの値をシンボルにて表記

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     sequence(:title){ |n| "やる事#{n}" }
     content { "本文" } 
-    status  { "todo" }
+    status  { :todo }
     association :user
   end
 end
